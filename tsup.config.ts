a7d8a1c5ad3cf40440bd5602fffe659683497874
Vyacheslav Ruzanov <spb.web@bk.ru>
2022-12-27 22:06:48 +0400

    v1.0.3

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,38 +1,15 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    name: 'pack-bytes-to-utf8',
-    entry: ['src'],
-    sourcemap: true,
-    clean: true,
-    minify: false,
-    outDir: './dist/esm',
-    format: 'esm',
-    platform: 'browser',
-    treeshake: true,
-    bundle: true,
-  }, {
-    name: 'pack-bytes-to-utf8',
-    entry: ['src'],
-    sourcemap: true,
-    clean: true,
-    minify: false,
-    outDir: './dist/cjs',
-    format: 'cjs',
-    platform: 'browser',
-    treeshake: true,
-    bundle: false,
-  },
-  {
-    name: 'pack-bytes-to-utf8',
-    entry: ['src'],
-    sourcemap: true,
-    clean: true,
-    minify: false,
-    outDir: './dist/types',
-    format: [],
-    dts: true,
-    bundle: false,
-  },
-])
+export default defineConfig({
+  name: 'pack-bytes-to-utf8',
+  entry: ['src'],
+  sourcemap: true,
+  clean: true,
+  minify: false,
+  outDir: './dist',
+  format: ['esm', 'cjs'],
+  platform: 'browser',
+  treeshake: true,
+  bundle: true,
+  dts: true,
+})
